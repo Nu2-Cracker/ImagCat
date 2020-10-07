27517cba78f7589dfb908386bc1cf62e8970c199
Ryoji Uehara <orcinusorca1758dv6932@gmail.com>
2020-10-07 23:35:18 +0900

    画像リンク切れの有無のチェック

--- src/ImagCat.nim
+++ src/ImagCat.nim
@@ -5,12 +5,23 @@ import xmltree  # To use '$' for XmlNode
 import strtabs  # To access XmlAttributes
 import strutils # To use cmpIgnoreCase
 
-proc getImage(url: string): bool =
 
+type
+  LinkCheckResult = ref object
+    link: string
+    state: bool
+
+proc checkLink(link: string): LinkCheckResult =
   var client = newHttpClient()
+  try:
+    return LinkCheckResult(link:link, state:client.get(link).code == Http200)
+  except:
+    return LinkCheckResult(link:link, state:false)
 
+
+proc getImage(url: string): bool =
+  var client = newHttpClient()
   downloadFile(client, url, "log.html")
-
   var html = loadHtml("log.html")
 
   for img in html.findAll("img"):
@@ -18,11 +29,15 @@ proc getImage(url: string): bool =
       let (dir, filename, ext) = splitFile(img.attrs["src"])
       if ext == ".jpg" or ext == ".JPG":
         var imgURL = "https:" & img.attrs["src"]
-        var filename = filename & ".png"
-        downloadFile(client, imgURL, "output/" & filename)
+        var result = checkLink(imgURL)#画像urlがリンク切れしていないかどうかチェック
+        if result.state:
+          var filename = filename & ".png"
+          downloadFile(client, imgURL, "output/" & filename)
 
 
   echo "downloaded!!"
+
+
 
 
 echo "Please give me one URL!"
